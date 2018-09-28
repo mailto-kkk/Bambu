e3e2846718edd306a664b8cc426d1f6d4f63607c
Kumaresan <k.kumaresan@welcome-rt.com>
2018-09-28 23:02:54 +0800

    feature/Include_Coordinates_As_Suggestion - Read values from config values, instead of hard coding

--- suggestionAPI/src/controllers/v1.js
+++ suggestionAPI/src/controllers/v1.js
@@ -7,67 +7,11 @@ var suggestionService = require('../services/suggestionService');
 var fs = require('fs');
 var parse = require('csv-parse');
 var lodash = require('lodash');
+
 module.exports = function (router) {
 
-
-
-    //Get All Merchant-Campaigns
     router.get(constants.URI_PEOPLE_LIKE_YOU, function(req, res){
         logger.msg('INFO', 'v1', '', '', 'GET ' + constants.URI_PEOPLE_LIKE_YOU, 'GET All people like you');
-        var inputPath="data/test.csv";
-        var rs = fs.createReadStream(inputPath);
-        var matchedRecords;
-        var name=req.query.name;
-        var age=parseInt(req.query.age);
-        var monthlyIncome=parseInt(req.query.monthlyIncome);
-        var latitude=parseInt(req.query.latitude);
-        var longitude=parseInt(req.query.longitude);
-        var experienced=req.query.experienced;
-        if(latitude && !longitude){
-            // Both Lat and Long has to come in pairs. or it is an error scenario
-            logger.msg('INFO', 'v1', '', '', 'GET ', 'Only latitude is present. so error condition');
-            return commonUtil.sendResponseWoBody(res, httpStatus.BAD_REQUEST);
-        }
-        if(!latitude && longitude){
-            // Both Lat and Long has to come in pairs. or it is an error scenario
-            logger.msg('INFO', 'v1', '', '', 'GET ', 'Only longitude is present. so error condition');
-            return commonUtil.sendResponseWoBody(res, httpStatus.BAD_REQUEST);
-        }
-
-        // Later get the below value from Config file.
-        var graceAge=10;
-        var graceMonthlyIncome=500;
-
-        var parser = parse({columns: true,skip_empty_lines: true}, function(err, data){
-
-
-            var finalData = data.reduce((finalData, iteratedData) => {
-                    iteratedData.age=parseInt(iteratedData.age);
-                    iteratedData.monthlyIncome=parseInt(iteratedData.monthlyIncome);
-                    suggestionService.computeAgeSuggestion(age,iteratedData,graceAge,finalData)
-                    .then(function (finalData){
-                        return suggestionService.computeMonthlyIncomeSuggestion(monthlyIncome,iteratedData,graceMonthlyIncome,finalData);
-                    }).then(function (finalData) {
-                        return ;
-
-                    },function (err) {
-                        logger.msg('ERROR', 'v1', '', '', 'controllers', 'Undefined error in controllers - ' + err.stack);
-                        return commonUtil.sendResponseWoBody(res, httpStatus.INTERNAL_SERVER_ERROR);
-                    });
-
-                    /*if(monthlyIncome && iteratedData.monthlyIncome >= monthlyIncome-graceMonthlyIncome && iteratedData.monthlyIncome <= monthlyIncome+graceMonthlyIncome){
-                        iteratedData.score="NeedToCompute";
-                        iteratedData.searchField="monthlyIncome";
-                        finalData.push(iteratedData);
-                    }*/
-
-            return finalData;
-        }, []);
-            return commonUtil.sendResponse(res, httpStatus.OK,finalData);
-        });
-        rs.pipe(parser);
+        suggestionService.getSuggestedData(req,res);
     });
-
-
-
 };
--- suggestionAPI/src/lib/constants.js
+++ suggestionAPI/src/lib/constants.js
@@ -3,5 +3,6 @@
 module.exports = {
     'URI_PEOPLE_LIKE_YOU': '/people-like-you',
     'CON_TYPE_UTF': 'application/json; charset=utf-8',
-    'CON_TYPE': 'application/json'
+    'CON_TYPE': 'application/json',
+    'SEEDING_DATA': 'data/test.csv'
 };
